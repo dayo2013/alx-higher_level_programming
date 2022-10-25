f7f3c2e00539c1fbdf3388dcb8a97bcaeec8b3a0
dayo2013 <emmanuelagbaje501@gmail.com>
2022-10-25 15:41:34 +0000

    1. Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-def number_of_lines(filename=""):
-    with open(filename, encoding='utf-8') as f:
-        return len(f.readlines())
+number_of_lines = __import__('1-number_of_lines').number_of_lines
+
+filename = "my_file_0.txt"
+nb_lines = number_of_lines(filename)
+print("{} has {:d} lines".format(filename, nb_lines))
