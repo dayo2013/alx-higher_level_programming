ff908c740dd205c3c70ab33b40697793d0dd0687
dayo2013 <emmanuelagbaje501@gmail.com>
2022-10-25 15:33:28 +0000

    1. Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
 def number_of_lines(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        return len(list(f))
+    with open(filename, encoding='utf-8') as f:
+        return len(f.readlines())
