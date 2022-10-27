ed07e87670c954da24eba1a9c4cc8d34611d1234
dayo2013 <emmanuelagbaje501@gmail.com>
2022-10-27 14:43:50 +0000

    8. Class to JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+"""
+Contains the "class_to_json" function
+"""
 
 
-import json
+def class_to_json(obj):
+    """returns the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object"""
+    return obj.__dict__
 
-
-def load_from_json_file(filename):
-    with open(filename, "r", encoding="UTF-8") as f:
-        return json.load(f)
