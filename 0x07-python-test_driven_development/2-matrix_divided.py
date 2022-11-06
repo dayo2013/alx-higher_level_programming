8a954642cd53cfde3073f9c5acc3a4b40b368521
dayo2013 <emmanuelgbaje501@gmail.com>
2022-11-06 02:24:14 -0800

    2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
-    errorMessage = "matrix must be a matrix (list of lists) of integers/floats"
-    if not matrix:
-        raise TypeError(errorMessage)
-    if not isinstance(matrix, list):
-        raise TypeError(errorMessage)
-    for lists in matrix:
-        if not isinstance(lists, list):
-            raise TypeError(errorMessage)
-        for item in lists:
-            if not isinstance(item, int) and not isinstance(item, float):
-                raise TypeError(errorMessage)
-    for lists in matrix:
-        if len(lists) == 0:
-            raise TypeError(errorMessage)
-    if not isinstance(div, int) and not isinstance(div, float):
-        raise TypeError("div must be a number")
-    if not all(len(lists) == len(matrix[0]) for lists in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
+    s = 'matrix must be a matrix (list of lists) of\
+ integers/floats'
+    if not matrix or type(matrix) is not list:
+        raise TypeError(s)
+    for row in matrix:
+        if type(row) is list and row:
+            for ele in row:
+                if type(ele) is not int and type(ele) is not float:
+                    raise TypeError(s)
+        else:
+            raise TypeError(s)
+
+    row_size = len(matrix[0])
+    for row in matrix:
+        if len(row) != row_size:
+            raise TypeError('Each row of the matrix must have the same size')
+    if type(div) is not int and type(div) is not float:
+        raise TypeError('div must be a number')
     if div == 0:
-        raise ZeroDivisionError("division by zero")
-    return [[round(item / div, 2) for item in lists] for lists in matrix]
+        raise ZeroDivisionError('division by zero')
+
+    matrix2 = list(map(list, matrix))
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
+            matrix2[row][col] = round(matrix[row][col] / div, 2)
+
+    return matrix2
